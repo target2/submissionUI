52cfba4ecf02a7e71255f358300daa63ecfaa6c8
Daofeng Li <lidaof@gmail.com>
2017-05-05 12:25:37 -0500

    bulk upload

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.youtube']
+#extensions = ['sphinxcontrib.youtube']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'submissionUI'
-copyright = u'2016, Erica, Deepak, Daofeng, Ting'
-author = u'Erica, Deepak, Daofeng, Ting'
+copyright = u'2017, Erica, Deepak, Xiaoyu, Daofeng, Ting'
+author = u'Erica, Deepak, Xiaoyu, Daofeng, Ting'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
